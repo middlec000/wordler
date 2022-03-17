68397d45dc5be751bc4c2b46c00274aa07456573
Colin Middleton <46512256+middlec000@users.noreply.github.com>
2022-03-16 19:00:55 -0700

    requirements: removed re

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 Program to play the game Wordle.
 
 # Data Sources
+## Kagle Dataset
+https://www.kaggle.com/rtatman/english-word-frequency  
+Accessed: 03/07/2022  
 ## Word Frequencies
 https://www.wordfrequency.info/  
 Free sample of 'Top 60,000 lemmas + word forms (100,000+ forms)'  
 https://www.wordfrequency.info/samples.asp  
 Accessed: 03/06/2022  
-## Kagle Dataset
-https://www.kaggle.com/rtatman/english-word-frequency  
-Accessed: 03/07/2022
 ## NLTK English Dictionary
 https://www.nltk.org/index.html  
 Accessed via:  
@@ -18,10 +18,9 @@ NLTK Version: 3.6.5
 
 # TODO
 * Figure out initial letter-getting mode.  
-  * Try as many different vowels as possible.
+  * Try as many vowels as possible.
 * Streamlit Website
   * Fix double-letter inconsistency with Wordle - get extra info
-  * Make input checker more robust
   * Mode 1: Help me with Wordle - suggest words
   * Mode 2: Play World for me
   * Mode 3: Guess my word
--- src/main.py
+++ src/main.py
@@ -4,29 +4,30 @@ from helper_methods import *
 
 def main():
     # Get Data
-    datapath = 'Data-Preprocessed/word_freq.csv' # TODO: update w/url
+    # datapath = 'Data-Preprocessed/word_freq_wordle_only.csv'
+    datapath = 'https://raw.githubusercontent.com/middlec000/wordler/main/Data-Preprocessed/word_freq_wordle_only.csv'
     data = pd.read_csv(datapath).sort_values(by='wordFreq', ascending=False)
-    data_length = len(data)
 
     # User Instructions
-    st.write('# The Wordler: Here to help you cheat at Wordle!')
+    st.write('# The Wordler')
+    st.write('## Here to help you win (cheat) at Wordle!')
     st.write('Here is the Wordle website: [https://www.nytimes.com/games/wordle/index.html](https://www.nytimes.com/games/wordle/index.html)')
 
-    st.write('Enter the five letter Wordle word, then the feedback you get from Wordle in the text boxes below:')
+    st.write('Enter each of your five letter Wordle guesses, a dash (-), then the feedback you get from Wordle in the text boxes below:')
     col1, col2 = st.columns(2)
     with col1:
         st.write('* 0 for black\n* 1 for yellow\n* 2 for green')
     with col2:
         st.write('Example Entry:')
         st.write('hello-10112')
-    st.write('NOTE: For the example word of "hello", if there is a single "l" in the actual word, The Wordler will expect BOTH "l"s to have "1"s, but Wordle will only give the first "l" a "1". For now, you should enter the feedback for both "l"s as "1"s.')
+    st.write('NOTE: For the example word of "hello", if there is a single "l" in the answer word, The Wordler will expect BOTH "l"s to have "1"s, but Wordle will only give the first "l" a "1". For now, you should enter the feedback for both "l"s as "1"s.')
     st.write('## Enter Wordle Guesses and Feedback Below')
     col3, col4 = st.columns(2)
     user_inputs = []
     with col4:
         # Get user input
         for i in range(1,7):
-            st.write(f'### Word {i}')
+            st.write(f'### Guess {i}')
             user_input = st.text_input(label=f'Word{i}-XXXXX',value='', max_chars=11).upper()
             if user_input:
                 user_inputs.append(user_input)
@@ -39,11 +40,10 @@ def main():
         # Display Filtered Data
         if guesses and not bad_input:
             filtered_data = filter_words(guesses=guesses, words=data)
-            data_length = len(filtered_data)
-            st.write(f'Total Number of Words Remaining: {data_length}')
+            st.write(f'Total Number of Words Remaining: {len(filtered_data)}')
             st.table(filtered_data.iloc[:24])
         else:
-            st.write(f'Total Number of Words Remaining: {data_length}')
+            st.write(f'Total Number of Words Remaining: {len(data)}')
             st.table(data.iloc[:24])
     return
 
